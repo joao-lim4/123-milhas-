cdccea8f38202e393b5ac7745b1f2d4d12c7c316
Lima <cl4sh.of.cl4ans2016@gmail.com>
2021-11-01 03:09:20 -0300

    all

--- app/Helpers/FlightHelper.php
+++ app/Helpers/FlightHelper.php
@@ -6,33 +6,43 @@ namespace App\Helpers;
 
 class FlightHelper {
 
+    /**
+     * @var outbounds
+     */
     private $outbounds = [
         "size"      => 0,
         "data"      => [],
         "groupFare" => []
     ];
 
+     /**
+     * @var flights
+     */
     private $flights = [];
 
+     /**
+     * @var inbounds
+     */
     private $inbounds = [
         "size"      => 0,
         "data"      => [],
         "groupFare" => []
     ];
 
+    /**
+     * @var groups
+    */
     private $groups = [];
 
+    /**
+     * @var infos
+    */
     private $infos = [
         "totalGroups"   => 0,
         "totalFlights"  => 0,
         "cheapestPrice" => 999999,
         "cheapestGroup" => 0
     ];
-
-    // “totalGroups”: // quantidade total de grupos
-    // “totalFlights”: // quantidade total de voos únicos
-    // “cheapestPrice”: // preço do grupo mais barato
-    // “cheapestGroup”: // id único do grupo mais barato
 
     public function __construct(array $flights)
     {
@@ -44,7 +54,13 @@ class FlightHelper {
         $this->organizePrices();
     }
 
-
+    /**
+     * initGroups separa todos os voos em um array contendo somente
+     * voos de ida e outro array contendo voos de volta.
+     *
+     * @var inbounds voos de volta
+     * @var outbounds voos de ida
+     */
     private function initGroups (array $flights, int $initPostion)
     {
         if($flights[$initPostion]["outbound"] === 1) {
@@ -62,6 +78,8 @@ class FlightHelper {
         return $this->initGroups($flights, ($initPostion - 1));
     }
 
+
+
     private function initFareGroup(string $fareKey, array $flight, string $objectThis): void
     {
         if(array_key_exists($fareKey, $this->{$objectThis}["groupFare"])) {
@@ -73,6 +91,11 @@ class FlightHelper {
         $this->{$objectThis}["groupFare"][$fareKey]["data"][] = $flight;
     }
 
+
+    /**
+     * Gera os primeiros grupos de voos, contendo
+     * somente voos de ida.
+     */
     private function generateInitialOutboundGroup(array $DataGroupFareOut, array $data, int $sizeOut): array
     {
 
@@ -90,7 +113,15 @@ class FlightHelper {
         return $this->generateInitialOutboundGroup($DataGroupFareOut, $data, ($sizeOut - 1));
     }
 
-
+    /**
+     * Caso a quantidade de grupos que tem somente um voo de ida
+     * seja maior que a quantidade de voos de volta distribuirei todos
+     * os voos de volta randomicamente entre os grupos somente com voos de ida
+     * e os grupos que ficarem sem o voo de volta, eu agruparei juntamente a outros grupos
+     * ja completos.
+     *
+     * Podendo assim um grupo ter mais de uma opcao de ida.
+     */
     private function spreadRoundTripFlights(array $DataGroupFare, array $flights, int $flightsSize, int $sizeDataGroupFare): array
     {
         $arraySort = range(0, ($sizeDataGroupFare - 1));
@@ -129,6 +160,11 @@ class FlightHelper {
         return $DataGroupFare;
     }
 
+
+    /**
+     * Apos gerar os grupos de voos somente com ida,
+     * distribuirei todas os voos de volta
+     */
     private function spreadReturnFlights(array $DataGroupFare, array $flights, int $flightsSize): array
     {
         $sizeDataGroupFare = sizeof($DataGroupFare);
@@ -152,11 +188,14 @@ class FlightHelper {
     }
 
 
+    /**
+     * Ira separar e gerar todos os grupos de voos
+     */
     private function separateGroupFlight(array $GroupFareOut, array $GroupFareIn): void
     {
         $groups = [];
 
-        //grups somente com viagens de ida
+        //gupos somente com viagens de ida
         foreach($GroupFareOut as $FareOutKey => $FareOut) {
             $groups[$FareOutKey] = $this->generateInitialOutboundGroup($FareOut["data"], [], ($FareOut["size"] - 1));
             $groups[$FareOutKey] = $this->spreadReturnFlights($groups[$FareOutKey], $GroupFareIn[$FareOutKey]["data"], $GroupFareIn[$FareOutKey]["size"]);
@@ -165,7 +204,11 @@ class FlightHelper {
         $this->groups = $groups;
     }
 
-
+    /**
+     * Ao finalizar de gerar todos os grupos
+     * e gerada novas informacoes gerais
+     * sobre os grupos
+     */
     private function changeInfos(): void
     {
         foreach($this->groups as $groups) {
@@ -184,7 +227,11 @@ class FlightHelper {
         }
     }
 
-
+    /**
+     * Vai orgnaizar os grupos gerados
+     * em ordem crescente usando como chave
+     * o campo totalPrice
+     */
     private function organizePrices(): void
     {
         $newGroups = [];
